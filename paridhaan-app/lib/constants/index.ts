af23a1a8b623479aa9c7963e94c9ca669a4973ce
Jitender S Negi <jitendersingh.negi@lyreco.com>
2025-10-03 21:49:05 +0200

    Deleted the front end code

--- paridhaan-app/lib/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Paridhaan';
-export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'Paridhaal online fashion store';
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL  || 'http://localhost:3000';
