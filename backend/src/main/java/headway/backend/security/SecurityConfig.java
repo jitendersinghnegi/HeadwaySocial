ea652c8bcca44508a291dc6d7b52e64899b7cb33
Jitender S Negi <jitendersingh.negi@lyreco.com>
2025-10-03 22:21:51 +0200

    added the structure for backend

---
+++ backend/src/main/java/headway/backend/security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package headway.backend.security;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
---
+++ backend/src/test/java/headway/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package headway.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
